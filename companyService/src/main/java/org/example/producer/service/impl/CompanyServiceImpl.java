d583ab883c51db18b5571e86808df28711ab9f4f
igr76 <gritsuk766@gmail.com>
2025-03-31 19:47:37 +1000

    proseed WebClient

--- companyService/src/main/java/org/example/producer/service/impl/CompanyServiceImpl.java
+++ companyService/src/main/java/org/example/producer/service/impl/CompanyServiceImpl.java
@@ -10,7 +10,13 @@ import org.example.producer.entity.CompanyEntity;
 import org.example.producer.repository.CompanyRepositiry;
 import org.example.producer.service.CompanyService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientSsl;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.net.URL;
@@ -23,14 +29,22 @@ import java.util.stream.Collectors;
 @Service
 public class CompanyServiceImpl implements CompanyService {
 
+    private final RestClient.Builder restClientBuilder;
+    //  private final RestClient.Builder restClientBuilder;
     @Value("${applycation.user}")
     private String urlUser;
     private CompanyRepositiry companyRepositiry;
     private CompanyMapper companyMapper = new CompanyMapper();
+    private WebClient webClient;
 
-    public CompanyServiceImpl(CompanyRepositiry companyRepositiry) {
-        this.companyRepositiry = companyRepositiry;
+    public CompanyServiceImpl(RestClient.Builder restClientBuilder) {
+        this.restClientBuilder = restClientBuilder;
     }
+
+//    public CompanyServiceImpl(CompanyRepositiry companyRepositiry, RestClient.Builder restClientBuilder) {
+//        this.companyRepositiry = companyRepositiry;
+//        this.restClientBuilder = restClientBuilder;
+//    }
 
     @Override
     public CompanyDto getCompanyByName(String name) {
@@ -51,10 +65,16 @@ public class CompanyServiceImpl implements CompanyService {
         CompanyEntity companyEntity = new CompanyEntity();
         companyEntity = companyRepositiry.findByName(name).orElseThrow();
         CompanyDtoAndUsers companyDtoWithListEmployees = new CompanyDtoAndUsers();
+
         if (companyEntity != null) {
             companyDto = companyMapper.toDto(companyEntity);
+            webClient = WebClient.builder().baseUrl(urlUser+ "/user/all").build();
             List<Integer> list = companyDto.getListUser();
-            UserDto[] userDtos = objectMapper.readValue(new URL(urlUser + "/user/all"), UserDto[].class);
+            Mono< UserDto[]> response = webClient.get()
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .bodyToMono( UserDto[].class).log();
+            UserDto[] userDtos=response.block();
 
             Arrays.stream(userDtos)
                     .filter(e -> list.stream()
